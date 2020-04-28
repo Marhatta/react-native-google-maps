de5cb85072f9cecbc6de86b35022dc3964f7b815
Marhatta <vishal.marhatta@gmail.com>
2020-04-29 01:24:30 +0530

    :sparkles: Fetched current position using Geolocation

--- App.js
+++ App.js
@@ -1,31 +1,75 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, View, StatusBar, Dimensions} from 'react-native';
+/* eslint-disable prettier/prettier */
+
+import React, { Component } from 'react';
+import {SafeAreaView, StyleSheet, View, StatusBar, Dimensions, PermissionsAndroid} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={styles.container}>
-          <MapView
-            provider={PROVIDER_GOOGLE}
-            style={styles.map}
-            region={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.015,
-              longitudeDelta: 0.0121,
-            }}
-          />
-        </View>
-      </SafeAreaView>
-    </>
-  );
-};
+//DEFAULT LOCATION DATA
+const LATITUDE_DELTA = 0.009;
+const LONGITUDE_DELTA = 0.009;
+const LATITUDE = 18.7934829;
+const LONGITUDE = 98.9867401;
+
+class App extends Component {
+
+  constructor(props){
+      super(props);
+      this.state = {
+        latitude:LATITUDE,
+        longitude:LONGITUDE,
+        error:null,
+      }
+  }
+
+  componentDidMount() {
+    //Get the current location
+    Geolocation.getCurrentPosition(position => {
+      console.log('=============position===========',position);
+      this.setState({
+        latitude:position.coords.latitude,
+        longitude:position.coords.longitude,
+        error:null,
+      });
+    },
+      error => {
+        this.setState({error:error.message});
+      },
+      { enableHighAccuracy: true, timeout: 200000, maximumAge: 1000 }
+      );  
+  }
+
+
+  //Get the map region
+  getMapRegion = () => {
+    return {
+      latitude:this.state.latitude,
+      longitude:this.state.longitude,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    }
+  }
+
+  render(){
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <View style={styles.container}>
+            <MapView
+              provider={PROVIDER_GOOGLE}
+              style={styles.map}
+              region={this.getMapRegion()}
+            />
+          </View>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
